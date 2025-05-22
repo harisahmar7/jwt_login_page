fe4cd25e9b13058726b2129cd57999989db3c34f
harisahmar <haris.ahmar@firstcry.in>
2025-05-22 14:55:22 +0530

    Today commit

--- model/loginmodel.js
+++ model/loginmodel.js
@@ -3,5 +3,9 @@ const dbConnector = require('../middleware/dbconnection');
 module.exports = {
     getLoginUser(userObj){
         return dbConnector.query(`select * from login_master where username = '${userObj.username}'`);
+    },
+
+    getLoginUser1(userObj){
+        return dbConnector.query(`select * from login_master where username = '${userObj.username}'`);
     }
 }
